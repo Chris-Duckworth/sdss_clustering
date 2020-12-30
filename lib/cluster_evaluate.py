a20b15a1b274ebda38da1fb0660bfcce65f67436
Chris Duckworth <chrisduckworth@Chris-Mac.local>
2020-12-30 14:35:29 +0000

    implemented k-means on basic CNN, added file

--- lib/cluster_evaluate.py
+++ lib/cluster_evaluate.py
@@ -27,4 +27,18 @@ def plot_cluster(images, row_len=10):
     
     fig.subplots_adjust(wspace=0, hspace=0.05)
     return fig, ax
-    
+
+
+def save_cluster_plot(images, cluster_number, cluster_total, plot_path='./'):
+	'''
+	Given a set of images in a cluster, a cluster number (label), and the total number 
+	of clusters (req. for filename format.) and the directory to be save to, this creates
+	a complete grid of images for the cluster.
+	'''
+
+	fig, ax = plot_cluster(images)
+	# adding title to first subplot - suptitle for a variable number of rows moves relatively to images.
+	ax[0,0].set_title('Cluster {}, ({} galaxies)'.format(cluster_number, images.shape[0]), x=5, fontsize=35, ha='center')
+	plt.savefig(plot_path+'cluster_' + str(cluster_number) + '_of_' + str(cluster_total) +'.pdf',
+				format='pdf') ;
+	return 
